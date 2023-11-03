4b3e13d140a552ab3b370b221d318b28cdc6bd1a
Abhishek Verma <abhishekverma0203@gmail.com>
2023-11-03 13:57:35 +0530

    #6 Posts services

--- server/controllers/common.js
+++ server/controllers/common.js
@@ -3,10 +3,11 @@ const jwt = require("jsonwebtoken");
 
 const Messages = Object.freeze({
     Unauthorized: 'User not loggedIn',
-    FeedSuccess: 'Feed Success',
+    Success: 'Success',
     UserNotFound: 'User does not exists',
     InvalidCredentials: 'Invalid Credentials',
-    LoginSuccess: 'Login Successful'
+    PostNotFound: 'Post not found',
+    EmptyPost: 'Post content is empty'
 })
 
 const User = mongoose.model('User');
@@ -52,7 +53,7 @@ homeFeed = async (req, res, next) =>{
 
     res.status(200).json({
         code: 1006,
-        msg: Messages.FeedSuccess,
+        msg: Messages.Success,
         data:{
             user,
             posts
@@ -103,7 +104,7 @@ userFeed = async (req, res, next) =>{
 
     res.status(200).json({
         code: 1008,
-        msg: Messages.FeedSuccess,
+        msg: Messages.Success,
         data:{
             user,
             posts
@@ -112,7 +113,95 @@ userFeed = async (req, res, next) =>{
 
 }
 
+getPost = async (req, res, next) =>{
+    // verify token
+    const token = req.cookies.token;
+
+    var decode = tokenVerification(token);
+    if(!decode){
+        res.status(401).json({code:200,msg:Messages.Unauthorized,data:{}});
+        return;
+    }
+
+    var user = await User.findOne({_id:decode.id});
+    if(!user){
+        res.status(404).json({code:200,msg:Messages.UserNotFound,data:{}});
+        return;
+    }
+
+    const paramId = req.params.id;
+    var post = null;
+    if(paramId) {
+        try{
+            post = await Post.findOne({_id:paramId});
+        }
+        catch(e){
+            post = null;
+        }
+        
+        if(!post) { 
+            res.status(404).json({
+                code: 1009,
+                msg: Messages.PostNotFound,
+                data:{}
+            });
+            return;
+        }
+    }
+
+    res.status(200).json({
+        code: 1010,
+        msg: Messages.Success,
+        data:{
+            post
+        }
+    })
+
+}
+
+addPost = async (req, res, next) =>{
+    // verify token
+    const token = req.cookies.token;
+
+    var decode = tokenVerification(token);
+    if(!decode){
+        res.status(401).json({code:200,msg:Messages.Unauthorized,data:{}});
+        return;
+    }
+
+    var user = await User.findOne({_id:decode.id});
+    if(!user){
+        res.status(404).json({code:200,msg:Messages.UserNotFound,data:{}});
+        return;
+    }
+
+    if(!req.body){
+        res.status(200).json({code:1011,msg:Messages.EmptyPost,data:{}});
+        return;
+    }
+    var postData = new Post({
+        ...req.body, 
+        from: user._id,
+        likes: '0',
+        comments: '0'
+    });
+    
+    const newPost = await postData.save();
+
+    res.status(200).json({
+        code: 1010,
+        msg: Messages.Success,
+        data:{
+            newPost,
+        }
+    })
+
+}
+
+
 module.exports = {
     homeFeed,
-    userFeed
+    userFeed,
+    getPost,
+    addPost
 };
--- server/routes/common.js
+++ server/routes/common.js
@@ -4,12 +4,14 @@ const cookieParser = require("cookie-parser")
 
 const User = require('../models/User');
 const Post = require('../models/Post');
-const {homeFeed,userFeed} = require('../controllers/common.js')
+const {homeFeed,userFeed,getPost, addPost} = require('../controllers/common.js')
 
 router.use(cookieParser());
 
 router.get('/home', homeFeed);
 router.get('/user', userFeed);
 router.get('/user/:id', userFeed);
+router.get('/post/:id', getPost);
+router.post('/post', addPost);
 
 module.exports = router;
